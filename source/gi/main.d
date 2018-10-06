8aeaa2860d25c6c9d8865f83827112a16737593e
fminkowski <fminkowski@gmail.com>
2018-10-06 19:37:17 -0400

    remove print

--- source/gi/main.d
+++ source/gi/main.d
@@ -34,16 +34,4 @@ void run(Commands cmds) {
 	foreach (line; llvm.line) {
 		lines ~= line;
 	}
-
-	auto prg = "@var = unnamed_addr constant [3 x i8] c\"%d\00\" align 1\n" ~
-			  "declare i32 @printf(i8*, ...)\n" ~ 
-			  "define i32 @main(i32, i8**) { \n" ~
-			  lines
-			 ~ "\n"
-			"call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @var, i32 0, i32 0), i32 %v)\n" ~
-			"ret i32 0 \n"
-			"}";
-
-	import file = std.file; file.write("c:\\Users\\fmink\\OneDrive\\Desktop\\out.ll", prg);
-	import std.stdio; writeln(prg);
 }
